88e20bd0a4fc8e438e0b648475b4993727220621
Muralidhar K <al4037@accionlabs.in>
2023-11-20 13:05:06 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  
   return (
     <div className="App">
-      
+
     </div>
   );
 }
